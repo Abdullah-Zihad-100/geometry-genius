a94a9424e162ffd4997be98749c0f34c3026d453
Abdullah-zihad-100 <zihadahmad60@gmail.com>
2024-03-15 02:34:24 +0600

    All card add end

--- script/Area-Calculator.js
+++ script/Area-Calculator.js
@@ -1,25 +1,24 @@
 // Basic simple code 
-
 // triangle --------------
-
 function calcualteTriangleArea(){
 //   get triangle base value 
 const baseField=document.getElementById('triangle-base');
 const baseValueText=baseField.value;
 const base=parseFloat(baseValueText);
-
 // get triangle height value 
 const heightField=document.getElementById('triangle-height');
 const heightValueText=heightField.value;
 const height=parseFloat(heightValueText);
-
 // calculate area 
 const area=0.5*height*base;
+// valid input
+if(isNaN(height)||isNaN(base)){
+    alert('enter valid number')
+    return;
+}
 // get area value
 const triangleArea=document.getElementById('triangle-area');
 triangleArea.innerText=area;
-
-
 // Rectangle --------------
 }
 function calcualteRactangleArea(){
@@ -28,41 +27,66 @@ function calcualteRactangleArea(){
  const widthValueText=widthField.value;
  const width=parseFloat(widthValueText);
 // get length
-
 const lengthField=document.getElementById('ractangle-length');
 const lengthValueText=lengthField.value;
 const length=parseFloat(lengthValueText);
-// console.log(length ,width)
-
 // caculate arae
 const area=width*length;
+// valid input
+if(isNaN(width)||isNaN(length)){
+    alert('enter valid number')
+    return;
+}
 // show recatangle are
 const recatangleArea=document.getElementById('rectangle-area');
 recatangleArea.innerText=area
 }
-
-
-
-
-
 // New function Mathod Use short code ----->
-
-
 // parallelogram---------------
-
 function calcualteParallelogramArea(){
    const base=getInputValue('parallelogram-base');
 const height=getInputValue('parallelogram-heigth');
 const area=base*height
+// valid input 
+if(isNaN(base)||isNaN(height)){
+    alert('enter valid number')
+    return;
+}
 setElementTextValue('parallelogram-area',area)
 }
-
-
-// ellipse--------------------
-
+// ellipse-------------------->
 function calcualteellipseArea(){
 const a=getInputValue('ellipse-a');
 const b=getInputValue('ellipse-b');
 const area=3.1415*a*b;
+// valid input 
+if(isNaN(a)||isNaN(b)){
+    alert('enter valid number')
+    return;
+}
 setElementTextValue('ellipse-area',area);
+}
+// Rhombus --------------->
+function calcualteRhombusArea(){
+const d1=getInputValue('rhombus-d1')
+const d2=getInputValue('rhombus-d2')
+const area=0.5*d1*d2;
+// valid input 
+if(isNaN(d1)||isNaN(d2)){
+    alert('enter valid number')
+    return;
+}
+setElementTextValue('rhombus-area',area)
+}
+// Pentagon  --------------->
+function calcualtePentagonArea(){
+const p=getInputValue('pentagon-p')
+const b=getInputValue('pentagon-b')
+const area=0.5*p*b;
+// valid input 
+if(isNaN(p)||isNaN(b)){
+    alert('enter valid number')
+    return;
+}
+setElementTextValue('pentagon-area',area)
 }
--- script/utiliti.js
+++ script/utiliti.js
@@ -1,25 +1,12 @@
-function calcualteParallelogramArea(){
-   const base=getInputValue('parallelogram-base');
-const height=getInputValue('parallelogram-heigth');
-const area=base*height
-setElementTextValue('parallelogram-area',area)
-}
-
-
 // reusebale function ---> reduce deplicate code 
-
 function getInputValue(fieldId){
     const inputField=document.getElementById(fieldId);
     const inputValueText=inputField.value;
     const value=parseFloat(inputValueText);
     return value;
 }
-
-
-
 // reuseble set span ,p,div,etc,text
 function setElementTextValue(elementId,area){
     const element=document.getElementById(elementId);
     element.innerText=area
-
 }
